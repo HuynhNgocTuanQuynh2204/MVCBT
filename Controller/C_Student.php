bf5c137b624714e516735b87879c392581241bfd
HuynhNgocTuanQuynh2204 <tuanquynh3452@gmail.com>
2023-11-08 15:15:33 +0700

    firstname

---
+++ Controller/C_Student.php
@@ -0,0 +1,20 @@
+<?php
+include_once("../Model/M_Student.php");
+
+class Ctrl_Student {
+    public function invoke() {
+        if (isset($_GET['stid'])) {
+            $modelStudent = new Model_Student();
+            $student = $modelStudent->getStudentDetail($_GET['stid']);
+            include_once("../View/StudentDetail.html");
+        } else {
+            $modelStudent = new Model_Student();
+            $studentList = $modelStudent->getAllStudent();
+            include_once("../View/StudentList.html");
+        }
+    }
+}
+
+$C_Student = new Ctrl_Student();
+$C_Student->invoke();
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang chủ</title>
+</head>
+
+<body>
+    <h1>Trang chủ</h1>
+    <p><a href="Controller/C_Student.php">Danh sách học sinh</a></p>
+</body>
+
+</html>
